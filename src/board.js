ecd3c884732e4fbd0ff2d490b6498774487686d6
yangxian <yangxian01@baidu.com>
2017-08-22 21:29:54 +0800

    first commit

---
+++ src/board.js
@@ -0,0 +1,163 @@
+class Board {
+    constructor() {
+        this.fourPercent = 0.2; // 4出现概率
+        this.cells = [];
+        this.tiles = [];
+        for(let row = 0; row < 4; row++) {
+            this.cells.push([]);
+            for(let column = 0; column < 4; column++) {
+                let newTile = new Tile();
+                this.cells[row][column] = newTile;
+                this.tiles.push(newTile);
+            }
+        }
+        this.addRandomTile();
+        this.addRandomTile();
+        this.setPosition();
+    }
+
+    addRandomTile() {
+        let seed = Math.random() * this.getEmptyNum();
+        seed = Math.floor(seed);
+        let emptyCells = [];
+        for(let row = 0; row < 4; row++) {
+            for(let column = 0; column < 4; column++){
+                if(!this.cells[row][column].value) {
+                    emptyCells.push({row:row,column:column});
+                }
+            } 
+        }
+        let cell = emptyCells[seed];
+        let newTile = new Tile(this.getRandomNum());
+        this.cells[cell.row][cell.column] = newTile;
+        this.tiles.push(newTile);
+    }
+    
+    getEmptyNum() {
+        let emptyNum = 0;
+        for(let row = 0; row < 4; row++) {
+            for(let column = 0; column < 4; column++) {
+                if(!this.cells[row][column].value) {
+                    emptyNum++;
+                }
+            }
+        }
+        return emptyNum;
+    }
+
+    getRandomNum() {
+        return Math.random() > this.fourPercent ? 2 : 4;
+    }
+
+    moveleft() {
+        let isChanged = false;
+        for(let row = 0; row < 4; row++) {
+            let list = this.cells[row].filter(tile => tile.value !== 0);
+            for(let i = 0; i < list.length; i++) {
+                isChanged |= (list[i].value !== this.cells[row][i].value)
+                if(i > 0 && list[i].value === list[i-1].value && !list[i-1].mergedInto) {
+                    let newTile = new Tile(list[i].value * 2);
+                    this.tiles.push(newTile);
+                    list[i-1].mergedInto = newTile;
+                    list[i].mergedInto = newTile;
+                    list.splice(i-1, 2, newTile);
+                    isChanged = true;
+                }
+            }
+            let emptyCells = 4 - list.length;
+            for(let i = 0; i < emptyCells; i++) {
+                let newTile = new Tile();
+                list.push(newTile);
+            }
+            this.cells[row] = list;
+        }
+        return isChanged;
+    }
+
+    clearOldTiles() {
+        this.tiles = this.tiles.filter(tile => !tile.markForDeletion);
+        this.tiles.forEach( tile => tile.markForDeletion = true);
+    }
+
+    setPosition() {
+        this.cells.map((Rowtiles,row) => {
+            Rowtiles.map((tile,column) => {
+                tile.oldRow = tile.row;
+                tile.oldColumn = tile.column;
+                tile.row = row;
+                tile.column = column;
+                tile.markForDeletion = false;
+            })
+        })
+        this.consoleValue();
+    }
+
+    rotateLeft(){
+        let res = [];
+        for(let row = 0; row < 4; row++) {
+            res.push([]);
+            for(let column = 0; column < 4; column++) {
+                res[row][column] = this.cells[column][3 - row];
+            }
+        }
+        return res;
+    }
+
+    move(direction) {
+        // 0 -> left, 1 -> up, 2 -> right, 3 -> down
+        this.clearOldTiles();
+        for(let i = 0; i < direction; i++) {
+            this.cells = this.rotateLeft();
+        }
+        let isMoved = this.moveleft()
+        for(let i = direction; i < 4; i++) {
+            this.cells = this.rotateLeft();
+        }
+        if(isMoved) {
+            this.addRandomTile();
+        }
+        this.setPosition();
+        return this;
+    }
+    // for debug
+    consoleValue() {
+        for(let row = 0; row < 4; row++) {
+            let rowList = this.cells[row];
+            console.log(rowList[0].value + " " + rowList[1].value + " " + rowList[2].value + " " + rowList[3].value)
+        }
+    }
+}
+
+class Tile {
+    constructor(value){
+        this.value = value || 0;
+        this.row = -1;
+        this.column = -1;
+        this.oldRow = -1;
+        this.oldColumn = -1;
+        this.markForDeletion = false;
+        this.mergedInto = null;
+    }
+
+    isNew() {
+        return this.oldRow == -1 && !this.mergedInto;
+    }
+
+    fromRow() {
+        return this.mergedInto ? this.row : this.oldRow;
+    }
+
+    fromColumn() {
+        return this.mergedInto ? this.column : this.oldColumn;
+    }
+
+    toRow() {
+        return this.mergedInto ? this.mergedInto.row : this.row;
+    }
+    
+    toColumn() {
+        return this.mergedInto ? this.mergedInto.column : this.column;
+    }
+}
+
+export {Board}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import './css/game.css'
+import './static/index.css';
+import './static/game.css';
+
+import {Board} from './board';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Board />, document.getElementById('root'));
+class BoardView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {board: new Board()}
+    }
+
+    componentDidMount() {
+        window.addEventListener("keydown",event => this.handleKeyDown(event));
+    }
+
+    componentWillMount() {
+        window.removeEventListener("keydown",event => this.handleKeyDown(event));
+    }
+    
+    handleKeyDown(event) {
+        if(this.state.board.getEmptyNum() === 0) return;
+        if(event.keyCode >=37 && event.keyCode <= 40) {
+            event.preventDefault();
+            let direction = event.keyCode - 37;
+            this.setState({board: this.state.board.move(direction)});
+        }
+    }
+
+    render() {
+        let cells = [];
+        this.state.board.cells.map((rowList,row) => {
+            rowList.map((tile,column) => {
+                cells.push(<div className="cell" key={row * 4 + column}></div>);
+            })
+        })
+        console.log(this.state.board.tiles);
+        let tiles = this.state.board.tiles
+            .filter(tile => tile.value !== 0)
+            .map((tile,index) => <TileView tile={tile} key={index}></TileView>);
+        return (
+            <div className="game-board">
+                <div className="cell-board">{cells}</div>
+                {tiles}
+            </div>
+        )
+    }
+}
+
+class TileView extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        if(this.props.tile === nextProps.tile) {
+            return true;
+        }
+
+        return true;
+    }
+    render() {
+        let tile = this.props.tile;
+        let classArray = [];
+        classArray.push("tile");
+        classArray.push("tile" + tile.value);
+        classArray.push("position-" + tile.row + "-" + tile.column);
+        if(tile.isNew()){
+            classArray.push("new");
+        }
+        else {
+            if(tile.fromRow() !== tile.toRow()) {
+                classArray.push("row-from-" + tile.fromRow() + "-to-" + tile.toRow());
+            }
+            if(tile.fromColumn() !== tile.toColumn()) {
+                classArray.push("column-from-" + tile.fromColumn() + "-to-" + tile.toColumn());
+            }
+        }
+        let classes = classArray.join(" ");
+        return (
+            <div className={classes}>{tile.value}</div>
+        )
+    }
+}
+
+
+ReactDOM.render(<BoardView />, document.getElementById('root'));
 registerServiceWorker();
